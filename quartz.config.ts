4cbe1793eccfbf83636798d9e50b8cfbaf0238e7
chinhau <im.chinhau@gmail.com>
2025-08-16 17:56:39 -0700

    Add Google Analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-CTQ18QRNYN",
     },
     locale: "en-US",
     baseUrl: "https://chinhau.github.io",
